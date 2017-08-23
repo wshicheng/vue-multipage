cd3af654595fd40a1e8ca44851574cf57298007f
wshicheng <630030437@qq.com>
2017-08-23 18:11:52 +0800

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        app: './src/index.js'
     },
     devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
@@ -18,7 +19,8 @@ module.exports = {
             title: 'Output Management',
             template:'./src/temp/index.html'
         }),
-        new ManifestPlugin()
+        new ManifestPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
     module: {
         rules: [{
